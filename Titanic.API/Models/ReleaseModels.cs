e5dbf1f693705abdddf74b054c9601ba6ccafad4
Lekuru <contact@lekuru.xyz>
2026-01-05 00:17:42 +0100

    Add osu client release models

---
+++ Titanic.API/Models/ReleaseModels.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Titanic.API.Models
+{
+    public class TitanicReleaseModel
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("known_bugs")]
+        public string KnownBugs { get; set; }
+
+        [JsonProperty("supported")]
+        public bool Supported { get; set; }
+
+        [JsonProperty("preview")]
+        public bool Preview { get; set; }
+
+        [JsonProperty("downloads")]
+        public List<string> Downloads { get; set; }
+
+        [JsonProperty("screenshots")]
+        public List<string> Screenshots { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class OsuReleaseModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("version")]
+        public int Version { get; set; }
+
+        [JsonProperty("subversion")]
+        public int Subversion { get; set; }
+
+        [JsonProperty("stream")]
+        public string Stream { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("files")]
+        public List<OsuReleaseFile> Files { get; set; }
+    }
+
+    public class OsuReleaseFile
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("filename")]
+        public string Filename { get; set; }
+
+        [JsonProperty("file_version")]
+        public string FileVersion { get; set; }
+
+        [JsonProperty("file_hash")]
+        public string FileHash { get; set; }
+
+        [JsonProperty("filesize")]
+        public long Filesize { get; set; }
+
+        [JsonProperty("patch_id")]
+        public int? PatchId { get; set; }
+
+        [JsonProperty("url_full")]
+        public string UrlFull { get; set; }
+
+        [JsonProperty("url_patch")]
+        public string UrlPatch { get; set; }
+
+        [JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
+    }
+
+    public class OsuChangelogModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("time")]
+        public DateTime Time { get; set; }
+
+        [JsonProperty("build")]
+        public string Build { get; set; }
+    }
+
+    public class ModdedReleaseModel
+    {
+        [JsonProperty("identifier")]
+        public string Identifier { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("icon_url")]
+        public string IconUrl { get; set; }
+
+        [JsonProperty("creator")]
+        public UserModelCompact Creator { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+    }
+
+    public class ModdedReleaseEntryModel
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("mod_name")]
+        public string ModName { get; set; }
+
+        [JsonProperty("version")]
+        public string Version { get; set; }
+
+        [JsonProperty("stream")]
+        public string Stream { get; set; }
+
+        [JsonProperty("checksum")]
+        public string Checksum { get; set; }
+
+        [JsonProperty("download_url")]
+        public string DownloadUrl { get; set; }
+
+        [JsonProperty("changelog")]
+        public string Changelog { get; set; }
+
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
