6498ad954461a86edfc33bedd8d82fd2102050aa
Lekuru <contact@lekuru.xyz>
2026-01-05 00:14:16 +0100

    Add authentication requests

---
+++ Titanic.API/Requests/Auth/LoginRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Titanic.API.Models;
+
+namespace Titanic.API.Requests
+{
+    public class LoginRequest : APIRequest<TokenModel>
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public LoginRequest(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        protected override TokenModel Execute(TitanicAPI api)
+        {
+            TokenModel response = api.Post<TokenModel>("/account/login", null, new Dictionary<string, string>
+            {
+                { "Authorization", $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes($"{Username}:{Password}"))}" }
+            });
+            api.Token = response;
+            api.Token.ExpiresAt = DateTime.UtcNow.AddSeconds(api.Token.ExpiresIn).ToLocalTime();
+            return response;
+        }
+    }
+}
---
+++ Titanic.API/Requests/Auth/RefreshTokenRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Titanic.API.Models;
+
+namespace Titanic.API.Requests
+{
+    public class RefreshTokenRequest : APIRequest<TokenModel>
+    {
+        public string RefreshToken { get; set; }
+
+        public RefreshTokenRequest(string refreshToken)
+        {
+            RefreshToken = refreshToken;
+        }
+
+        protected override TokenModel Execute(TitanicAPI api)
+        {
+            TokenModel response = api.Post<TokenModel>("/account/refresh", null, new Dictionary<string, string>
+            {
+                { "Authorization", $"Bearer {RefreshToken}" }
+            });
+            api.Token = response;
+            api.Token.ExpiresAt = DateTime.UtcNow.AddSeconds(api.Token.ExpiresIn).ToLocalTime();
+            return response;
+        }
+    }
+}
